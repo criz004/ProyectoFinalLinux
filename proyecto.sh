97039586e70d0793c9f35bdc42ff280f90342257
criz004 <velacris123@gmail.com>
2025-04-17 14:16:42 -0600

    [criz004][add][main] Modificar archivo de proyecto principal

--- proyecto.sh
+++ proyecto.sh
@@ -4,5 +4,34 @@
 # - 
 # - Velasco Avila Cristopher
 
-pwd
-read comando
+clear
+command="null"
+cd ~
+
+while true;
+do
+    read -p "${USERNAME}@${HOSTNAME} $(pwd) -> " command
+    
+    case $command in
+
+    exit)
+        break
+        ;;
+
+    PATTERN_2)
+        echo STATEMENTS_2
+        ;;
+
+    PATTERN_N)
+        echo STATEMENTS_N
+        ;;
+
+    *)
+        $command
+        ;;
+    esac
+
+
+done
+
+echo "Final lol"
