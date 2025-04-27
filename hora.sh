6a13a0f19f41a34d899d535c17058f24f16017dc
criz004 <velacris123@gmail.com>
2025-04-26 19:23:09 -0600

    [criz004][modificar][main] Agregar logica para la zona horaria

--- hora.sh
+++ hora.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
 
-# Este script muestra la hora y la fecha con ayuda del archivo rtc. Despues se examina el valor con grep y las expresiones regulares
+# Leemos hora y fecha desde el archivo /proc/driver/rtc
+hora=$(grep -o -E -m 1 '[0-9]{2}:[0-9]{2}:[0-9]{2}' /proc/driver/rtc)
+fecha=$(grep -o -E -m 1 '[0-9]{4}-[0-9]{2}-[0-9]{2}' /proc/driver/rtc)
 
-echo "Hora: "$(cat /proc/driver/rtc | grep -o -E -m 1 '[0-9]{2}:[0-9]{2}:[0-9]{2}')
-echo "Fecha: "$(cat /proc/driver/rtc | grep -o -E -m 1 '[0-9]{4}-[0-9]{2}-[0-9]{2}')
+# Separamos la hora en sus tres componentes
+horas=${hora:0:2}
+minutos=${hora:3:2}
+segundos=${hora:6:2}
 
+# Separamos la fecha en tres componentes
+anio=${fecha:0:4}
+meses=${fecha:5:2}
+dias=${fecha:8:2}
 
+# Zona horaria nueva
+offset=-6
+
+hora_nueva=$((10#$horas + offset))
+
+# Ajustar fecha si es necesario
+
+if [ "$hora_nueva" -lt 0 ]; then
+    hora_nueva=$((hora_nueva + 24))
+    dias=$((10#$dias - 1))
+fi
+
+if [ "$hora_nueva" -ge 24 ]; then
+    hora_nueva=$((hora_nueva - 24))
+    dias=$((10#$dias + 1))
+fi
+
+echo "Hora: ${hora_nueva}:${minutos}:${segundos}"
+echo "Fecha: ${anio}-${meses}-${dias}"
